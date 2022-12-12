8f959ba2f06178f3206b86934f2553a09fbe2092
its_DC <96367023+Devottam2809@users.noreply.github.com>
2022-12-12 23:36:30 +0530

    Create Sort_Colours_DNFsort.cpp
    DNF sort is used to sort an array consisting of 0's , 1's ,2's.
    Check the value of middle element of the array (a[mid]) :
    1) if 0 : swap(arr[low],arr[mid]) , low++ ,mid++
    2) if 1 :  mid++
    3) if 2 : swap(arr[mid],arr[high]) , high--

---
+++ Sort_Colours_DNFsort.cpp
@@ -0,0 +1,23 @@
+void sortColors(vector<int>& a) {
+        int two=a.size()-1;
+        int zero=0;
+    
+     for(int i=0;i<=two;)
+    {
+        if(a[i]==2)
+        {
+            swap(a[i],a[two]);
+            two--;
+        }
+        else if(a[i]==0)
+        {
+            swap(a[i],a[zero]);
+            zero++;
+            i++;
+        }
+        else{
+            i++;
+        }
+    }
+    }
+};
