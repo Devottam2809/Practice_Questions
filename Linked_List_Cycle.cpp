72d65c7ed7a23351899ecebe967eeef7048c7826
its_DC <96367023+Devottam2809@users.noreply.github.com>
2022-12-23 09:55:34 +0530

    Create Linked_List_Cycle.cpp

---
+++ Linked_List_Cycle.cpp
@@ -0,0 +1,18 @@
+// 141 Leet Code
+// Linked List Cycle @devottam2809
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(slow && fast && fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
