281982a0cbe42d845fb88fc92c08f965b85ea2f3
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-02-10 19:36:51 +0530

    Create Basic_Calculator_II.py

---
+++ Basic_Calculator_II.py
@@ -0,0 +1,11 @@
+// basic Calculator  2   Leetcode 227    @devottam2809
+
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        p=eval(s)
+        return p
