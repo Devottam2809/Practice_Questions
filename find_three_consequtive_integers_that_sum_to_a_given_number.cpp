f8c800e5e2381f93d0a78a799447d83ee76b580d
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-02-14 09:00:51 +0530

    Create find_three_consequtive_integers_that_sum_to_a_given_number.cpp

---
+++ find_three_consequtive_integers_that_sum_to_a_given_number.cpp
@@ -0,0 +1,19 @@
+// Find Three Consecutive Integers That Sum to a Given Number     Leetcode2177    @devottam2809
+
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+    vector<long long >ans;
+    vector<long long >zero;
+        long long int number=(num+3)/3;
+    ans.push_back(number-2);
+    ans.push_back(number-1);
+    ans.push_back(number);
+    long long int sum=0;
+        for(long long int i=0;i<ans.size();i++)
+            sum+=ans[i];
+    if(sum==num)
+        return ans;
+return zero;
+    }
+};
