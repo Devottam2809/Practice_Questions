df9b70c959876e9787868eb96ba313cc1bbeafb6
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-02-15 09:11:16 +0530

    Create Multiply_Strings.py

---
+++ Multiply_Strings.py
@@ -0,0 +1,10 @@
+# Multiply Strings      Leetcode 43     @devottam2809
+
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1)*int(num2))
