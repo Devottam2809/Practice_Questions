effdc085cd97253f140115eb0c290d08b6b85a65
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-02-20 18:38:40 +0530

    Create Invert_BST.cpp

---
+++ Invert_BST.cpp
@@ -0,0 +1,15 @@
+// Invert Binary Tree     Leetcode 226      @devottam2809
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+            return root;
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode *temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+    return root;
+    }
+};
