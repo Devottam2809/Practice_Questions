433b37b1049879fa6a79181bfd171dcc1496910d
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-04-10 06:50:08 +0530

    Create Valid_Parantheses.cpp

---
+++ Valid_Parantheses.cpp
@@ -0,0 +1,27 @@
+// Valid parantheses Leetcode 20    @devottam2809
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stk;
+        for(auto i: s)
+        {
+            if(i=='(' || i=='{' || i=='[')
+                stk.push(i);
+            else{
+                    if(!stk.empty()){
+                        char x=stk.top();
+                        if(i==')' && x=='(' || i=='}' && x=='{' || i==']' && x=='[' )
+                            stk.pop();
+                        else
+                            return false;
+                    }
+                    else{
+                        return false;
+                    } 
+                }
+            }
+        if(stk.empty())
+            return true;
+    return false;
+    }
+};
