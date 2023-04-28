0e1fdb2befcfbf98be15c93c1c4f3e0c21b19cce
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-04-28 21:49:15 +0530

    Create  Longest_Consecutive_Sequence.cpp

---
+++ Longest_Consecutive_Sequence.cpp
@@ -0,0 +1,24 @@
+//  Longest Consecutive Sequence    Leetcode 128    @devottam2809
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        map<int,int>m;
+        int result=0;
+        for(auto i: nums)
+            m[i]++;
+
+        for(auto i: m){
+            if(m.find(i.first-1) == m.end()){
+                int it=1;
+                int counter=1;
+                while(m.find(i.first+it)!=m.end()){
+                    counter++;
+                    it++;
+                }
+                result = max(result,counter);
+            }
+        }
+    return result;
+    }
+};
