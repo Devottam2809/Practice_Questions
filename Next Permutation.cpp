d6c47a74a7e964a531ecc521a2a3c780742b1c8f
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-06-08 23:29:49 +0530

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,8 @@
+// Leetcode 31    Next Permutation    @devottam2809
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+    }
+};
